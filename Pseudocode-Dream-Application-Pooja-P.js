36c997c26920d0534a9f5a7be36391c60d696df6
Pooja_Pandey <poojaam84@gmail.com>
2025-03-31 23:34:43 -0500

    Final assignment Pseudocode-Dream-Application-Pooja-P

---
+++ Pseudocode-Dream-Application-Pooja-P.js
@@ -0,0 +1,12 @@
+//Pseudocode for building the drinking water reminder app in every 2 hours  everyday.
+
+ 
+// 1.Declare variable 'userName' to store the user's name
+
+//calculate the timeduration in miliseconds 
+
+// Define reminder interval (in milliseconds, 2 hours)
+
+//2.Construct reminder message by combining "Hey " + userName + ", it's time to drink water!"
+  
+//notes- code related to these steps are demonstrated in project.js file.
